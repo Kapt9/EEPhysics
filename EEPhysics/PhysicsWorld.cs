c5074148dc9b2f28607c1f7a674be5d3f498afbd
jelpk000 <jelpk000@gmail.com>
2015-02-26 19:42:16 +0200

    Collision crash bug

--- EEPhysics/PhysicsWorld.cs
+++ EEPhysics/PhysicsWorld.cs
@@ -495,7 +495,11 @@ namespace EEPhysics
                     {
                         if (ItemId.CanJumpThroughFromBelow(tileId))
                         {
-                            int rot = blockData[x][y][0];
+                            int rot;
+                            if (blockData[x][y] == null)
+                                rot = 1;
+                            else
+                                rot = blockData[x][y][0]; ;
                             if (tileId == ItemId.OnewayCyan || tileId == ItemId.OnewayPink || tileId == ItemId.OnewayRed || tileId == ItemId.OnewayYellow)
                             {
                                 if ((p.SpeedY < 0 || a <= p.overlapy) && rot == 1)
