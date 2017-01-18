5e6a3321faa2ddc1665c6f3346b423d8326e4685
Jesse <eejesse@hotmail.co.uk>
2017-01-18 18:51:07 +0100

    Increase version

--- EEPhysics/Properties/AssemblyInfo.cs
+++ EEPhysics/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.4.5")]
-[assembly: AssemblyFileVersion("1.4.4.5")]
+[assembly: AssemblyVersion("1.4.4.6")]
+[assembly: AssemblyFileVersion("1.4.4.6")]
